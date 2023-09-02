f6730070abbaf7d4d09db20d7fc9f6a89062cfbb
César Fabián Rincón Robayo <crinconro@unal.edu.co>
2023-09-02 02:38:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
 import database as db
+
+
 app = Flask(__name__)
 
 @app.route('/')
