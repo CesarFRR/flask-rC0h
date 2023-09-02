27d3ee715b0c3d3f988587ce39e207f435ae5093
César Fabián Rincón Robayo <crinconro@unal.edu.co>
2023-09-02 02:42:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,24 @@ app = Flask(__name__, template_folder = template_dir)
 def index():
   return render_template('index.html')
 
+
+@app.route('/persona')
+def persona():
+    print('PETICION /persona !!')
+    cursor = db.database.cursor()
+    cursor.execute("SELECT * FROM personas")
+    myresult = cursor.fetchall()
+    #Convertir los datos a diccionario
+    insertObject = []
+    columnNames = [column[0] for column in cursor.description]
+    for record in myresult:
+        insertObject.append(dict(zip(columnNames,  [x if x!=None else " " for x in record])))
+    cursor.close()
+    return render_template('persona.html', data=insertObject)
+
+
+
+
+
 if __name__ == '__main__':
   app.run(port=5000)
